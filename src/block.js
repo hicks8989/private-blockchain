10aa86fb5be1ce1a44f225887f02c2e73f676963
Eric Hicks <eric.hicks.8989@gmail.com>
2020-05-27 17:45:59 -0400

    Created `validate()` method for `Block` class
    - Created validation for `Block` class by checking if current hash is the same as the calculated hash.

--- src/block.js
+++ src/block.js
@@ -22,14 +22,14 @@ class Block {
 		this.time = 0;                                              // Timestamp for the Block creation
 		this.previousBlockHash = null;                              // Reference to the previous Block Hash
     }
-    
+
     /**
      *  validate() method will validate if the block has been tampered or not.
      *  Been tampered means that someone from outside the application tried to change
      *  values in the block data as a consecuence the hash of the block should be different.
      *  Steps:
      *  1. Return a new promise to allow the method be called asynchronous.
-     *  2. Save the in auxiliary variable the current hash of the block (`this` represent the block object)
+     *  2. Save the block hash in auxiliary variable the current hash of the block (`this` represent the block object)
      *  3. Recalculate the hash of the entire block (Use SHA256 from crypto-js library)
      *  4. Compare if the auxiliary hash value is different from the calculated one.
      *  5. Resolve true or false depending if it is valid or not.
@@ -39,20 +39,22 @@ class Block {
         let self = this;
         return new Promise((resolve, reject) => {
             // Save in auxiliary variable the current block hash
-                                            
+            const blockHash = self.hash;
             // Recalculate the hash of the Block
-            // Comparing if the hashes changed
-            // Returning the Block is not valid
-            
-            // Returning the Block is valid
-
+            const calculatedHash = SHA256(JSON.stringify(self)).toString();
+            // Compare if the hashes changed
+            if (blockHash === calculatedHash) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
         });
     }
 
     /**
      *  Auxiliary Method to return the block body (decoding the data)
      *  Steps:
-     *  
+     *
      *  1. Use hex2ascii module to decode the data
      *  2. Because data is a javascript object use JSON.parse(string) to get the Javascript Object
      *  3. Resolve with the data and make sure that you don't need to return the data for the `genesis block` 
